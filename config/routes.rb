d51da3ccb7b8bfc86bf6743d1454eab61a9c7770
yuler <is.yuler@gmail.com>
2023-11-01 00:13:19 +0800

    Create user model

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      # We are going to list our resources here
+    end
+  end
 end
